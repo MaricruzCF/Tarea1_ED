0b10b93b8fef5e2dfb7954899687335016704339
Maricruz Michel <mmichel5@ucol.mx>
2018-02-23 14:25:13 -0600

    Tarea 1

---
+++ tarea-1ED/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace tarea_1ED
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtNivelA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtNivelM = new System.Windows.Forms.TextBox();
+            this.lblDirec = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtFecha2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(364, 57);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Procesar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 57);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Entrada:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 57);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // txtNivelA
+            // 
+            this.txtNivelA.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtNivelA.Location = new System.Drawing.Point(345, 175);
+            this.txtNivelA.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNivelA.Multiline = true;
+            this.txtNivelA.Name = "txtNivelA";
+            this.txtNivelA.Size = new System.Drawing.Size(73, 124);
+            this.txtNivelA.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 135);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sensores";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(341, 135);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Nivel";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(473, 135);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Direccion";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(-6, 364);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(721, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "_________________________________________________________________________________" +
+    "________";
+            // 
+            // txtNivelM
+            // 
+            this.txtNivelM.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtNivelM.Location = new System.Drawing.Point(345, 234);
+            this.txtNivelM.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNivelM.Multiline = true;
+            this.txtNivelM.Name = "txtNivelM";
+            this.txtNivelM.Size = new System.Drawing.Size(73, 65);
+            this.txtNivelM.TabIndex = 11;
+            // 
+            // lblDirec
+            // 
+            this.lblDirec.AutoSize = true;
+            this.lblDirec.Location = new System.Drawing.Point(575, 179);
+            this.lblDirec.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDirec.Name = "lblDirec";
+            this.lblDirec.Size = new System.Drawing.Size(13, 20);
+            this.lblDirec.TabIndex = 12;
+            this.lblDirec.Text = " ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 409);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Fecha";
+            // 
+            // txtFecha2
+            // 
+            this.txtFecha2.Location = new System.Drawing.Point(136, 544);
+            this.txtFecha2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtFecha2.Name = "txtFecha2";
+            this.txtFecha2.Size = new System.Drawing.Size(148, 26);
+            this.txtFecha2.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 470);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 40);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Cambiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(477, 166);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 115);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(42, 175);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(127, 115);
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(198, 175);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(127, 115);
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(84, 409);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(263, 26);
+            this.dateTimePicker1.TabIndex = 20;
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(184, 322);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(222, 26);
+            this.txtFecha.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 584);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtFecha2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblDirec);
+            this.Controls.Add(this.txtNivelM);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNivelA);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNivelA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtNivelM;
+        private System.Windows.Forms.Label lblDirec;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtFecha2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtFecha;
+    }
+}
+
---
+++ tarea-1ED/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tarea_1ED.Properties;
+
+namespace tarea_1ED
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt16(textBox1.Text);
+            num >>= 1;
+            int direccion = (num & 7),
+                nivel = ((num >>= 3) & 3),
+                sensor1 = ((num >>= 2) & 1),
+                sensor2 =((num >>= 1) & 1),
+                dia = ((num >>= 1) & 31),
+                mes = ((num >>= 5) & 15),
+                anio = ((num >>= 4) & 127);
+
+            switch (direccion)
+            {
+                case 0:
+                    pictureBox1.Image = Properties.Resources.N;
+                    break;
+                case 1:
+                    pictureBox1.Image = Properties.Resources.NE;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.E;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.SE;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.S;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.SO;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.O;
+                    break;
+                default:
+                    pictureBox1.Image = Properties.Resources.NO;
+                    break;
+            }
+
+            txtNivelM.Text = "";
+            switch (nivel)
+            {
+                case 0:
+                    txtNivelM.Visible = false;
+                    txtNivelA.BackColor = Color.Gray;
+                    break;
+                case 1:
+                    txtNivelM.Visible = true;
+                    txtNivelA.BackColor = Color.Gray;
+                    txtNivelM.BackColor = Color.FromArgb(0, 0, 123);
+                    break;
+                case 2:
+                    txtNivelM.Visible = false;
+                    txtNivelA.BackColor = Color.FromArgb(0, 0, 123);
+                    break;
+                default:
+                    txtNivelM.Visible = true;
+                    txtNivelM.Text = "///////////////////////////////////////////////////";
+                    txtNivelM.ForeColor = Color.FromArgb(0, 0, 123);
+                    txtNivelA.BackColor = Color.Gray;
+                    txtNivelM.BackColor = Color.Gray;
+                    break;
+            }
+
+            if (sensor1 == 1)
+                pictureBox2.Image = Properties.Resources.ok_simbolo_dentro_de_un_esquema_circulo_318_41760;
+            else
+                pictureBox2.Image = Properties.Resources.Prohibido;
+
+            if (sensor2 == 1)
+                pictureBox3.Image = Properties.Resources.ok_simbolo_dentro_de_un_esquema_circulo_318_41760;
+            else
+                pictureBox3.Image = Properties.Resources.Prohibido;
+
+            txtFecha.Text = dia + "/" + mes + "/" + (anio + 1900);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime fecha = dateTimePicker1.Value;
+            txtFecha2.Text = (fecha.Day | (fecha.Month << 5) | ((fecha.Year - 1900) << 9)).ToString();
+        }
+    }
+}
+//lblSenP.BackColor = Color.FromArgb(0,128,0);
+//lblSenN.BackColor = Color.FromArgb(252,0,0);
+//if (g >= 2)
+//{
+//    txtNivelM.Visible = false;
+//    txtNivelA.BackColor = Color.FromArgb(0, 0, 123);
+//}
+//else
+//{
+//    txtNivelM.Visible = true;
+//    txtNivelA.BackColor = Color.FromArgb(255,255,255);
+//    txtNivelM.BackColor = Color.FromArgb(0, 0, 123);
+//}
